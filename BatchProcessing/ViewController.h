98e9f63cb200c4f9bdae972204937a4bcc546211
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-06-30 14:24:11 -0300

    Initial Commit

---
+++ BatchProcessing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BatchProcessing
+//
+//  Created by Marcelo Sampaio on 6/30/15.
+//  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
